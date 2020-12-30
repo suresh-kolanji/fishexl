95243796b7672762cef9c5b6981a19cfbaba7380
Suresh K <suresh.k-cic1@ibm.com>
2020-12-30 17:06:19 +0100

    jjssss

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -5,6 +5,9 @@ import {
   Card,
   ListGroup,
   CardColumns,
+  Row,
+  Col,
+  Container,
   blockquote,
 } from "react-bootstrap";
 import { CartContext } from "./../../Context/CartContext";
@@ -19,6 +22,7 @@ const Cart = (props) => {
   ] = useContext(CartContext);
   console.log("----Default -Value of modal.----", CartShow);
   console.log("----Default -Value of Cart.----", Cart);
+
   return (
     <Modal
       size="lg"
@@ -26,25 +30,51 @@ const Cart = (props) => {
       centered
       show={CartShow}
     >
+      <Modal.Header>
+        <Modal.Title id="contained-modal-title-vcenter">
+          <h4>Shopping Cart</h4>
+        </Modal.Title>
+        <button type="button" onClick={() => setCartShow(false)} class="close">
+          <span aria-hidden="true">×</span>
+          <span class="sr-only">Close</span>
+        </button>
+      </Modal.Header>
       <Modal.Body>
-        <h4>Shopping Cart</h4>
         <p>
           <ListGroup variant="flush">
             {Cart.length
               ? Cart.map((Ca, i) => (
                   <ListGroup.Item>
-                    {" "}
-                    <li>{Ca.product.id}</li>
-                    {Ca.product.name},{Ca.product.description}{" "}
+                    <Container fluid>
+                      <Row>
+                        <Col> {i + 1}</Col>
+                        <Col>{Ca.product.name}</Col>
+                        <Col>{Ca.product.description}</Col>
+                        <Col>{Ca.product.constprice}</Col>
+                      </Row>
+                    </Container>
                   </ListGroup.Item>
                 ))
               : null}
+            <ListGroup.Item>
+              <Container fluid>
+                <Row>
+                  <Col>
+                    <Button outline-primary onClick={() => setCartShow(false)}>
+                      checkout
+                    </Button>
+                  </Col>
+                  <Col md={{ span: 3, offset: 3 }}>
+                    {Cart.map((datum) => datum.product.constprice).reduce(
+                      (a, b) => a + b
+                    )}
+                  </Col>
+                </Row>
+              </Container>
+            </ListGroup.Item>
           </ListGroup>
         </p>
       </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={() => setCartShow(false)}>Close</Button>
-      </Modal.Footer>
     </Modal>
   );
 };
